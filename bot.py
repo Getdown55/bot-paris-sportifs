462c269c20092ef93924024a766e133106127e9f
Getdown55 <yannickguedon@hotmail.fr>
2026-06-14 18:54:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 # ==========================================
 # CONFIGURATION & FILTRES PERSONNALISÉS
 # ==========================================
-TOKEN = "8625843812:AAGkWr-L7p3MQAmvWdIW0HYhki0ZJXeNPmc"
+TOKEN = os.environ.get("TELEGRAM_TOKEN", "8625843812:AAGkWr-L7p3MQAmvWdIW0HYhki0ZJXeNPmc")
+
 
 
 MOTS_CLES_CHAMPIONNATS = [
